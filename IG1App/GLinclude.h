a310c7ca0cb62a5d40041f3fd4a1d59280214d4a
Andres <47799692+AndresRuizB@users.noreply.github.com>
2020-06-10 11:57:18 +0200

    añadidos archivos

---
+++ IG1App/GLinclude.h
@@ -0,0 +1,8 @@
+#pragma once
+
+#include <Windows.h>
+// una de las siguientes dos líneas según la organización del proyecto
+
+#include "glad.h"
+#include <GL/freeglut.h> // OpenGL Utility Toolkit
+#include <glm.hpp> // OpenGL Mathematics
